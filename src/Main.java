7567e985f704f5ea8a0adc8f540458d494c8e4c0
andrey <thefrozenewr@gmail.com>
2020-04-15 17:47:57 +0300

    corrected resizable PQ

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,20 @@
 public class Main {
 
     static void LB_3_actions_BinaryHeap() {
+
         System.out.println("\n***LB 3 START\n");
         int MAX = 1_000_000;
         int[] a = new int[MAX];
         for (int i = 0; i < MAX; i++) {
             a[i] = StdRandom.uniform(1, 10);
         }
+
         System.out.println("Median of " + MAX + " el. = " + lb_3_BinaryHeap.getMedian(a));
-
-
         System.out.println("\n***LB 3 END\n");
     }
 
     static void LB_4_actions_RopeProblem() {
+
         System.out.println("\n***LB 4 START\n");
         int MAX = 1_000_000;
 //        int [] a = {4, 3, 2, 6};
@@ -22,6 +23,7 @@ public class Main {
         for (int i = 0; i < MAX; i++) {
             a[i] = StdRandom.uniform(1, 10);
         }
+
         System.out.println("Sum of " + MAX + " ropes = " + lb_4_Rope.getMinSum(a));
         System.out.println("\n***LB 4 END\n");
     }
